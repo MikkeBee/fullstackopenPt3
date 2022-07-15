da25f54dd0e67f5e808d23068fa92aaa9a984722
MikkeBee <mjblakley87@gmail.com>
2022-07-15 17:11:03 +0300

    uibuild

--- index.js
+++ index.js
@@ -19,44 +19,6 @@ app.use(
   )
 );
 
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: 2,
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: 3,
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: 4,
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ];
-
-// const randomIdMaker = () => {
-//   const maxNumber = 150;
-//   const randomNumber = Math.floor(Math.random() * maxNumber + 1);
-//   return randomNumber;
-// };
-
-// const generateId = () => {
-//   const maxId = persons.length > 0 ? randomIdMaker() : 0;
-//   return maxId + 1;
-// };
-
-// const countPeople = () => {
-//   return persons.length;
-// };
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
@@ -78,18 +40,18 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  // const id = Number(request.params.id);
-  // const person = persons.find((person) => person.id === id);
-
-  // if (person) {
-  //   response.json(person);
-  // } else {
-  //   response.status(404).end();
-  // }
-
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(500).end();
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
