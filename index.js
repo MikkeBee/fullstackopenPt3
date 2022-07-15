0323ce9efa388bb48fc3b24415f95b67931d045e
MikkeBee <mjblakley87@gmail.com>
2022-07-15 15:31:01 +0300

    add mongoose functions to backend

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require("http");
+require("dotenv").config();
 const express = require("express");
 const { response } = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
 app.use(express.json());
 app.use(cors());
@@ -17,67 +19,77 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+// let persons = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
 
-const randomIdMaker = () => {
-  const maxNumber = 150;
-  const randomNumber = Math.floor(Math.random() * maxNumber + 1);
-  return randomNumber;
-};
+// const randomIdMaker = () => {
+//   const maxNumber = 150;
+//   const randomNumber = Math.floor(Math.random() * maxNumber + 1);
+//   return randomNumber;
+// };
 
-const generateId = () => {
-  const maxId = persons.length > 0 ? randomIdMaker() : 0;
-  return maxId + 1;
-};
+// const generateId = () => {
+//   const maxId = persons.length > 0 ? randomIdMaker() : 0;
+//   return maxId + 1;
+// };
 
-const countPeople = () => {
-  return persons.length;
-};
+// const countPeople = () => {
+//   return persons.length;
+// };
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>The phonebook has info for ${countPeople()} people.</p><p>${new Date()}</p>`
-  );
+  Person.countDocuments().then((result) => {
+    response
+      .send(
+        `<p>The phonebook has info for ${result} people.</p><p>${new Date()}</p>`
+      )
+      .end();
+  });
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
+  // const id = Number(request.params.id);
+  // const person = persons.find((person) => person.id === id);
 
-  if (person) {
+  // if (person) {
+  //   response.json(person);
+  // } else {
+  //   response.status(404).end();
+  // }
+
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -93,23 +105,29 @@ app.post("/api/persons", (request, response) => {
     return response.status(400).json({
       error: "Some content is missing",
     });
-  } else if (persons.find((element) => element.name === body.name)) {
-    return response.status(400).json({
-      error: "That name already exists in the phonebook",
-    });
+    // } else if (persons.find((element) => element.name === body.name)) {
+    //   return response.status(400).json({
+    //     error: "That name already exists in the phonebook",
+    //   });
   }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.put("/api/persons/:id", (request, response) => {
+  const body = request.body;
+  const id = request.params.id;
+  const person = { name: body.name, number: body.number };
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.ce4yfgj.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+  return;
+}
+
+const name = process.argv[3];
+const number = process.argv[4];
+
+const person = new Person({ name, number });
+
+person.save().then((person) => {
+  console.log(`Added ${person.name} number ${person.number} to the phonebook`);
+  return mongoose.connection.close();
+});
